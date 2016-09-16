3cfd7030c28b0d03b257b4e7fe8cebf7b1433d4d
Łukasz Andrzejewski <lukasz.andrzejewski@inbright.pl>
2016-09-16 17:43:06 +0200

    MVP approach

--- app/src/main/java/pl/training/githubbrowser/model/github/Repository.java
+++ app/src/main/java/pl/training/githubbrowser/model/github/Repository.java
@@ -1,11 +1,8 @@
 package pl.training.githubbrowser.model.github;
 
-import android.os.Parcel;
-import android.os.Parcelable;
-
 import com.google.gson.annotations.SerializedName;
 
-public class Repository implements Parcelable{
+public class Repository {
 
     private long id;
     private String name;
@@ -18,22 +15,6 @@ public class Repository implements Parcelable{
     private String homepage;
     private User owner;
     private boolean fork;
-
-    public Repository() {
-    }
-
-    protected Repository(Parcel in) {
-        id = in.readLong();
-        name = in.readString();
-        description = in.readString();
-        forks = in.readInt();
-        watchers = in.readInt();
-        stars = in.readInt();
-        language = in.readString();
-        homepage = in.readString();
-        owner = in.readParcelable(User.class.getClassLoader());
-        fork = in.readByte() != 0;
-    }
 
     public long getId() {
         return id;
@@ -114,37 +95,5 @@ public class Repository implements Parcelable{
     public void setFork(boolean fork) {
         this.fork = fork;
     }
-
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeLong(id);
-        dest.writeString(name);
-        dest.writeString(description);
-        dest.writeInt(forks);
-        dest.writeInt(watchers);
-        dest.writeInt(stars);
-        dest.writeString(language);
-        dest.writeString(homepage);
-        dest.writeParcelable(owner, flags);
-        dest.writeByte((byte) (fork ? 1 : 0));
-    }
-
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    public static final Creator<Repository> CREATOR = new Creator<Repository>() {
-        @Override
-        public Repository createFromParcel(Parcel in) {
-            return new Repository(in);
-        }
-
-        @Override
-        public Repository[] newArray(int size) {
-            return new Repository[size];
-        }
-
-    };
 
 }
