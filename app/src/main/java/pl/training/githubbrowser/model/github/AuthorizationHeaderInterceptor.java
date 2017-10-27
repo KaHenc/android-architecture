8bce45039ffa9f6c99ac687d94cd221cb491ee5b
Łukasz Andrzejewski <lukasz.andrzejewski@inbright.pl>
2017-10-27 08:39:45 +0200

    Update

--- app/src/main/java/pl/training/githubbrowser/model/github/AuthorizationHeaderInterceptor.java
+++ app/src/main/java/pl/training/githubbrowser/model/github/AuthorizationHeaderInterceptor.java
@@ -11,7 +11,7 @@ public class AuthorizationHeaderInterceptor implements Interceptor {
     @Override
     public Response intercept(Chain chain) throws IOException {
         Request request = chain.request().newBuilder()
-                .addHeader("Authorization", "token ***")
+                .addHeader("Authorization", "token 2df245fcbcd9f675eea421fac74f3afac644fb7d")
                 .build();
         return chain.proceed(request);
     }
